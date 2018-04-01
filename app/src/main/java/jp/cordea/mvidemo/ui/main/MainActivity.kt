71c0cf20633423522ca432580c5d588effa69b92
Yoshihiro Tanaka <contact@cordea.jp>
2018-04-01 13:35:33 +0900

    fix layouts

--- app/src/main/java/jp/cordea/mvidemo/ui/main/MainActivity.kt
+++ app/src/main/java/jp/cordea/mvidemo/ui/main/MainActivity.kt
@@ -2,7 +2,6 @@ package jp.cordea.mvidemo.ui.main
 
 import android.os.Bundle
 import android.support.design.widget.NavigationView
-import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
@@ -31,11 +30,6 @@ class MainActivity : AppCompatActivity(),
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
