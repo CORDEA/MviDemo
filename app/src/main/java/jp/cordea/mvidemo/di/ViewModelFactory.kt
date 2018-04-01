0cde130ded5e7fb878125ed42290fe00fd40dcf0
Yoshihiro Tanaka <contact@cordea.jp>
2018-04-01 13:13:40 +0900

    simplify

--- app/src/main/java/jp/cordea/mvidemo/di/ViewModelFactory.kt
+++ app/src/main/java/jp/cordea/mvidemo/di/ViewModelFactory.kt
@@ -7,11 +7,9 @@ import javax.inject.Inject
 
 class ViewModelFactory<V : ViewModel> @Inject constructor(
         private val injector: MembersInjector<V>
-) : ViewModelProvider.Factory {
+) : ViewModelProvider.NewInstanceFactory() {
 
     @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T =
-            modelClass.newInstance().apply {
-                injector.injectMembers(this as V)
-            }
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =
+            super.create(modelClass).apply { injector.injectMembers(this as V) }
 }
